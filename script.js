c164b417e23f1940580a6e7778620edc0e85f859
zezelx <hazelrosemarie.pontilla@evsu.edu.ph>
2022-04-10 21:39:37 +0800

    bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
 const screen = document.querySelector('.screen');
 
 document.querySelector('.calc-buttons').addEventListener("click", function(event){
@@ -13,6 +13,7 @@ function buttonClick(value) {
     } else {
         handleNumber(value);
     }
+    rerender();
 }
 
 function handleNumber(value) {
@@ -21,11 +22,35 @@ function handleNumber(value) {
     } else {
         buffer += value;
     }
-    rerender();
 }
 
 function handleSymbol(value) {
-    
+    switch(value) {
+        case 'C':
+            buffer = "0";
+            runningTotal = 0;
+            previousOperator = null;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+                break;
+            }
+        default:
+            handleMath(value);
+            break;
+    }
 }
 
 function rerender() {
